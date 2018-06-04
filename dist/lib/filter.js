7ca4963edefd849a3cccfbeeb67be18fb6521c50
Jimmy Zhang <codgician@users.noreply.github.com>
2018-06-04 09:07:43 +0800

    Trim string before rendering

--- dist/lib/filter.js
+++ dist/lib/filter.js
@@ -5,13 +5,13 @@ const he_1 = require("he");
 function filter(data) {
     const displayRegExp = new RegExp("\\$\\$[^\\$]*\\$\\$", "mg");
     data.content = data.content.replace(displayRegExp, (s) => {
-        return katex_1.renderToString(he_1.unescape(s).slice(2, -2), {
+        return katex_1.renderToString(he_1.unescape(s).slice(2, -2).trim(), {
             displayMode: true
         });
     });
     const regexp = new RegExp("\\$([^\\$]*)\\$", "mg");
     data.content = data.content.replace(regexp, (s) => {
-        return katex_1.renderToString(he_1.unescape(s).slice(1, -1));
+        return katex_1.renderToString(he_1.unescape(s).slice(1, -1)).trim();
     });
     return data;
 }
