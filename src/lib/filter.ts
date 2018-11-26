76e755b6738c1c96c9d7fcf29fcb748761e2db2e
Jimmy Zhang <codgician@users.noreply.github.com>
2018-11-27 02:54:00 +0800

    Update katex to 0.10.0 (#1)
    * Upgrade dependencies
    * Fix wrong directory
    * Update tests
    * Bump version
    * Trim string before rendering
    * Update dependencies
    * Update dependencies
    * Update katex to 0.10.0

--- src/lib/filter.ts
+++ src/lib/filter.ts
@@ -4,14 +4,14 @@ import { unescape } from "he";
 export function filter(data: { content: string }) {
   const displayRegExp = new RegExp("\\$\\$[^\\$]*\\$\\$", "mg");
   data.content = data.content.replace(displayRegExp, (s: string) => {
-    return renderToString(unescape(s).slice(2, -2), {
+    return renderToString(unescape(s).slice(2, -2).trim(), {
       displayMode: true
     });
   });
 
   const regexp = new RegExp("\\$([^\\$]*)\\$", "mg");
   data.content = data.content.replace(regexp, (s: string) => {
-    return renderToString(unescape(s).slice(1, -1));
+    return renderToString(unescape(s).slice(1, -1)).trim();
   });
   return data;
 }
